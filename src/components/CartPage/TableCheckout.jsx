b2076be22a6009896e461b79c2094bd87559a9f5
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-29 02:14:49 -0300

    se soluciona error de renderizado de items en page checkout

--- src/components/CartPage/TableCheckout.jsx
+++ src/components/CartPage/TableCheckout.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 
 const TableCheckout = () => {
+
+    const [data, setdata] = useState([{}]);
+    let total = 0;
+
+    useEffect(() => {
+
+        setdata(JSON.parse(localStorage.getItem("cart")));
+
+
+    }, [])
+
     return (
 
         <div className='container mb-5 mt-5'>
@@ -16,20 +29,32 @@ const TableCheckout = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img src="" alt="" />
-                        </td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td><button className="btn btn-danger">Quitar</button></td>
-                    </tr>
+                    {
+                        data.map((i) => {
+                            total = total + i.price
+                            return (
+                                <>   <tr>
+                                    <td>
+                                        <img src={i.imgUrl} alt={i.name} height={"120px"} />
+                                    </td>
+                                    <td>{i.name}</td>
+                                    <td>$ {i.price}{ }</td>
+                                    <td><button className="btn btn-danger">Quitar</button></td>
+                                </tr>
+                                </>
+
+                            )
+                        }
+
+                        )
+                    }
+
                     <tr className='d-flex w-100 '>
-                    
+
                         <td className='d-flex w-25'>
                             <a href="" className="btn btn-primary fw-bold">Pagar con Mercado Pago</a>
                         </td>
-                        <td className='d-flex w-25'> <span>TOTAL = $ 2</span></td>
+                        <td className='d-flex w-25'> <span>TOTAL = $ {total}</span></td>
 
                     </tr>
                 </tbody>
