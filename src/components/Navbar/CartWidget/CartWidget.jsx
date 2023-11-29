4ffd953e002049cb89241fb039ebd91017fd9ea7
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-28 23:56:35 -0300

    se crea dropdown

--- src/components/Navbar/CartWidget/CartWidget.jsx
+++ src/components/Navbar/CartWidget/CartWidget.jsx
@@ -1,15 +1,22 @@
 import { BsCart4 } from 'react-icons/bs';
-import { useCart } from '../../../context/Hooks.js'; 
+import { useCart } from '../../../context/Hooks.js';
 import styles from './CartWidget.module.css'; // Importar archivo CSS
 
 const CartWidget = () => {
   const { totalProducts } = useCart();
 
   return (
+
+
+
     <div className={styles.cartWrapper}>
-      <div className={styles.cartIcon}>
+      <div className={styles.cartIcon} data-bs-toggle="dropdown" aria-expanded="false">
         <BsCart4 />
       </div>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+
+      </ul>
       {totalProducts > 0 && ( // Mostrar el contador solo si hay productos en el carrito
         <div className={styles.productCount}>
           {totalProducts}
