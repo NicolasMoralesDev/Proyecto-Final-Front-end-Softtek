67229d7c18109a8a1406baa789d82416a8f079ec
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-29 17:30:16 -0300

    se agregan mejoras en el diseño del carrito

--- src/components/Navbar/CartWidget/CardCart/CardCart.jsx
+++ src/components/Navbar/CartWidget/CardCart/CardCart.jsx
@@ -11,38 +11,38 @@ const CardCart = () => {
 
     useEffect(() => {
 
-          setdata(JSON.parse(localStorage.getItem("cart")));
-    
-     
+        setdata(JSON.parse(localStorage.getItem("cart")));
+
+
     }, [cart])
-    
-    
+
+
 
     return (
         data ?
-        <div className='container_items_dropdown' style={{ maxHeight: '200px' }}>
+            <div className='container_items_dropdown' style={{ maxHeight: '200px' }}>
 
-       {
-        data.map(i => (
-            <div className="card mb-3 mt-3 dropdown-item" style={{ maxWidth: '700px' }} key={i.name}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={i.imgUrl} className="img-fluid  h-100 w-100 rounded-start" alt={i.name}></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">{i.name}</h5>
-                            <p className="card-text fw-bold">$ {i.price }</p>
+                {
+                    data.map(i => (
+                        <div className="card mb-3 mt-3 dropdown-item" style={{ maxWidth: '700px' }} key={i.name}>
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img src={i.imgUrl} className="img-fluid  h-100 w-100 rounded-start" alt={i.name}></img>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{i.name}</h5>
+                                        <p className="card-text fw-bold">$ {i.price}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        )
+                    )
 
-        )} 
-        </div>
-: <></>
-    ) 
+                    )}
+            </div>
+            : <h6 className='text-center'>Sin Productos</h6>
+    )
 }
 
 export default CardCart
