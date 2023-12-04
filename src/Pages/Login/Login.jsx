6546404aa40b283d349af87ed11aa067ab7c7346
Nicolás Muros <nicomuros@gmail.com>
2023-12-04 18:12:21 -0300

    login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,22 @@
 import LoginForm from '../../components/LoginForm/LoginForm'
+import RegisterForm from '../../components/RegisterForm/RegisterForm'
 import { Helmet } from 'react-helmet'
 import { useUser } from '../../context/Hooks';
 import { Col, Row } from 'react-bootstrap';
 import styles from './Login.module.css'
+import { useNavigate } from 'react-router';
 
 const Login = () => {
-    const { login } = useUser();
+    const { login, register } = useUser();
+    const navigate = useNavigate();
 
-    const handleSubmit = (values) => {
+    const handleLoginSubmit = (values) => {
         login(values.email, values.password);
+        navigate('/');
+    }
+    const handleRegisterSubmit = (values) => {
+        register(values);
+        navigate('/');
     }
     return (
         <>
@@ -18,9 +26,14 @@ const Login = () => {
                 <link rel="canonical" href="http://mysite.com/example" />
             </Helmet>
             <div className={`container ${styles.container}`}>
-                <Row className="d-flex justify-content-center">
-                    <Col xs={6} lg={6} xl={4}>
-                        <LoginForm handleSubmit={handleSubmit}/>
+                <Row className="d-flex justify-content-around">
+                    <Col xs={7} lg={5} xl={3}>
+                        <h1 style={{padding: "30px"}}>Iniciar sesión</h1>
+                        <LoginForm handleSubmit={handleLoginSubmit}/>
+                    </Col>
+                    <Col xs={7} lg={5} xl={3}>
+                        <h1 style={{padding: "30px"}}>Registrarse</h1>
+                        <RegisterForm handleSubmit={handleRegisterSubmit}/>
                     </Col>
                 </Row>
                 
