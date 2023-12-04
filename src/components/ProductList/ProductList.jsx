21fd04be12ebffe8b9864d1de142971692fc1e0f
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-12-04 20:35:53 -0300

    se soluciona error de renderizado

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -11,8 +11,7 @@ const ProductList = () => {
     const { cart, addToCart } = useCart();
     const { page } = useContext(PaginationContext);
 
-    const [ products, setProducts] = useState([{}]);
-
+    const [products, setProducts] = useState([{}]);
     const moveToCart = (product) => {
         product = { ...product, imageUrl: product.imgUrl }; //a
         addToCart(product, 1);
@@ -38,34 +37,38 @@ const ProductList = () => {
 
     return (
         <>
-             {products.map((i) => (
-                <div className="card mb-3 mt-3" style={{ maxWidth: '540px' }} key={i.product.id*i.product.id}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src={i.product.imageUrl} style={{maxWidth: "200px", maxHeight: "200px", aspectRatio: "auto"}}  className="h-100 img-fluid rounded-start" alt={i.product.name}></img>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">{i.product.name}</h5>
-                                <p>{i.product.brand}</p>
-                                <p className="card-text fw-bold">Precio $ {i.product.price}</p>
-                                <Toaster
-                                    position="bottom-right"
-                                    reverseOrder={false}
-                                />
-                                <button  className="btn text-light btn-orange-custom mt-2 fw-bold" onClick={() => moveToCart(i)}>agregar al carrito</button>
-                                <button className="btn text-light mt-2 bg-success fw-bold">ver</button>
+            {
+                products.product ?
+                    products.map((i) => (
+                        <div className="card mb-3 mt-3" style={{ maxWidth: '540px' }} key={i.product.id * i.product.id}>
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img src={i.product.imageUrl} style={{ maxWidth: "200px", maxHeight: "200px", aspectRatio: "auto" }} className="h-100 img-fluid rounded-start" alt={i.product.name}></img>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{i.product.name}</h5>
+                                        <p>{i.product.brand}</p>
+                                        <p className="card-text fw-bold">Precio $ {i.product.price}</p>
+                                        <Toaster
+                                            position="bottom-right"
+                                            reverseOrder={false}
+                                        />
+                                        <button className="btn text-light btn-orange-custom mt-2 fw-bold" onClick={() => moveToCart(i)}>agregar al carrito</button>
+                                        <button className="btn text-light mt-2 bg-success fw-bold">ver</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>)
-            )
-            } 
+                        </div>)
+                    )
+
+                    : <></>
+            }
             <div className="container-fluid d-flex justify-content-center align-items-center mt-5 mb-5">
-               <PaginationProduts/> 
+                <PaginationProduts />
             </div>
-            
-            
+
+
         </>
 
     )
