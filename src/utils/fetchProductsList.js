ee974dc3dbcfb3e8e4900d91b7a4495ca0c93ed5
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-12-09 20:44:32 -0300

    se soluciona problema responsive pagina change password

--- src/utils/fetchProductsList.js
+++ src/utils/fetchProductsList.js
@@ -5,7 +5,6 @@ export const getAllProducts = async (page = 0) => {
     try {
 
         const response = await axiosConf.get(`public/products?page=${page}`);
-        console.log("Respuesta de la API:", response.data);
         return response.data;
 
     } catch (error) {
