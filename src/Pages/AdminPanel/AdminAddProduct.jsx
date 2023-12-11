0bd9afce5592508da4d1baa995358de35c461c58
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-12-11 16:36:17 -0300

    se termina de implementar cloundinary

--- src/Pages/AdminPanel/AdminAddProduct.jsx
+++ src/Pages/AdminPanel/AdminAddProduct.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { addProduct } from '../../utils/fetchProductsList';
+import UploadWidget from '../../components/cloundinary/UploadWidget';
 
 const AdminAddProduct = ({ onAddProduct }) => {
+
     const [productData, setProductData] = useState({
         price: '',
         brand: '',
@@ -12,7 +14,15 @@ const AdminAddProduct = ({ onAddProduct }) => {
         stock: '',
     });
 
+    const [url, updateUrl] = useState();
+    const [error, updateError] = useState();
+
+    productData.imageUrl = url;
+
     const handleInputChange = (e) => {
+
+        productData.imageUrl = url;
+
         const { name, value } = e.target;
         setProductData((prevData) => ({
             ...prevData,
@@ -46,6 +56,24 @@ const AdminAddProduct = ({ onAddProduct }) => {
         }
     };
 
+
+
+    /**
+     * handleOnUpload
+     */
+
+    function handleOnUpload(error, result, widget) {
+        if (error) {
+            updateError(error);
+            widget.close({
+                quiet: true
+            });
+            return;
+        }
+        updateUrl(result?.info?.secure_url);
+
+    }
+
     return (
         <div className='container'>
             <h2>Agregar Producto</h2>
@@ -71,8 +99,21 @@ const AdminAddProduct = ({ onAddProduct }) => {
                     <label htmlFor="floatingPrice">Precio</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" placeholder="Url Imagen" id='floatingImage' value={productData.imageUrl} name='imageUrl' onChange={handleInputChange} />
-                    <label htmlFor="floatingImage">Url Imagen</label>
+
+                    <UploadWidget onUpload={handleOnUpload}>
+                        {({ open }) => {
+                            function handleOnClick(e) {
+                                e.preventDefault();
+                                open();
+                            }
+                            return (
+                                <button onClick={handleOnClick}>
+                                    Subir imagen
+                                </button>
+                            )
+                        }}
+                    </UploadWidget>
+
                 </div>
 
                 <div className="form-floating mb-3">
