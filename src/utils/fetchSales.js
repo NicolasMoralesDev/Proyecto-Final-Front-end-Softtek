2cb1b71baf1d83be8a42c1fc5ce7cf5f0cae1ab8
tomaslenzi <tomaslenzi93@gmail.com>
2023-12-11 18:14:01 -0300

    correccion return getUserSales

--- src/utils/fetchSales.js
+++ src/utils/fetchSales.js
@@ -41,7 +41,7 @@ export const getUserSales = async (idUser, page=0) => {
     try {
         const response = await axiosConf.post(`user/sale/all?page=${page}`, idUser);
         console.log(response.data)
-        return response.data;
+        return response;
     } catch (error) {
         console.error("Error al obtener las ventas del usuario", error);
         throw error;
